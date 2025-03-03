63b44f76231120c8ddb4cec7a968c789d91b8111
SrRiuz <mateo.jimenez@alternova.com>
2025-03-03 13:01:40 -0500

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { JSX } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./Screens/Home";
+
+function App(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
