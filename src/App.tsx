7951eb20bbe8169030d1d09fab07eacf597807c4
SrRiuz <mateo.jimenez@alternova.com>
2025-03-26 16:38:29 -0500

    Adding landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { JSX } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./Screens/Home";
+import { Container } from "@mui/material";
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
